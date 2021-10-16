319d229dfc08c65fa35d1fb55d6c4ef890bdfbe0
QIanZhang1218 <55051157+QIanZhang1218@users.noreply.github.com>
2021-10-17 12:58:43 +1300

    init project

---
+++ TicketWebApplication/TicketWebApplication/Controllers/Checkout.cs
@@ -0,0 +1,49 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Stripe;
+
+namespace Library.WebApi.Controllers
+{
+    [ApiController]
+    [Route("create-payment-intent")]
+    public class PaymentIntentApiController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(PaymentIntentCreateRequest request)
+        {
+            var paymentIntents = new PaymentIntentService();
+            var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
+            {
+                Amount = CalculateOrderAmount(request.Items),
+                Currency = "nzd",
+            });
+            return Json(new {clientSecret = paymentIntent.ClientSecret});
+        }
+
+        private int CalculateOrderAmount(Item[] items)
+        {
+            // Replace this constant with a calculation of the order's amount
+            // Calculate the order total on the server to prevent
+            // people from directly manipulating the amount on the client
+            return (int) (items[0].totalAmount*100);
+        }
+        public class Item
+        {
+            [JsonProperty("id")]
+            public string Id { get; set; }
+            [JsonProperty("penalty")]
+            public decimal totalAmount { get; set; }
+        }
+        public class PaymentIntentCreateRequest
+        {
+            [JsonProperty("items")]
+            public Item[] Items { get; set; }
+        }
+    }
+}
